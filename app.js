5354647d4b4565e4a70287015979c66aa55c6ce4
HitzSPB <daniel.oesterbro@gmail.com>
2021-08-02 23:18:22 +0200

    Added setup of app

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const data = require("./data.json").projects;
 const express = require("express");
+
+// Added set for pug to use it as a view engine
+app.set("view engine", "pug"); 
+app.set('views', './views');
+app.use('/static', express.static('public'));
