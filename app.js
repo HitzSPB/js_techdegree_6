964a9406d24cd1b8c5378b3c120b91c2681e9208
HitzSPB <daniel.oesterbro@gmail.com>
2021-08-03 01:29:15 +0200

    fixed the page to exceed expcetations

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const data = require("./data.json").projects;
 app.set("view engine", "pug"); 
 app.set('views', './views');
 app.use('/static', express.static('public'));
+// Added images otherwise the page will not show images on the index when loaded in through data
+app.use('/images', express.static('images'));
+
 
 // When index route is called
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {data});
   });
   
 // When about route is called
@@ -19,8 +22,30 @@ app.get('/about', (req, res) => {
 
   // Getting a project
   app.get('/project/:id', (req, res) => {
-      res.render('project');
+      res.render('project', {data, data: data[req.params.id]});
     });
+
+
+    // Must be below above otherwise all reqests are failing
+    app.use((req, res, next) => {
+        console.log('404 error handler called')
+        const error = new Error('The requested page could not be found');
+        error.code = 404; 
+        next(error);
+    })
+    app.use((error, req, res, next) => {
+        if(error.code === 404)
+        {
+            res.status(error.code); 
+            res.render('page-not-found.pug', {error: error}); //render error in error template
+        }
+        else
+        {
+            error.message || "Something went wrong on the server"
+            res.status(error.code || 500); 
+            res.render('error', {error: error}); //render error in error template
+        }
+    })
 
   // Listen to port 300
 app.listen(3000, () => {
