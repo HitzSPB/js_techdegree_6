31cd8d274aac1f4636925f5bca2ec3244a00f943
HitzSPB <daniel.oesterbro@gmail.com>
2021-08-02 23:35:16 +0200

    Added basic code to make the application running and being able to call the basic sites like index.

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const data = require("./data.json").projects;
 const express = require("express");
+const app = express();
+const data = require("./data.json").projects;
 
 // Added set for pug to use it as a view engine
 app.set("view engine", "pug"); 
 app.set('views', './views');
 app.use('/static', express.static('public'));
+
+// When index route is called
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+  
+// When about route is called
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+  // Getting a project
+  app.get('/project/:id', (req, res) => {
+      res.render('project');
+    });
+
+  // Listen to port 300
+app.listen(3000, () => {
+    console.log('Application running on port 3000');
+  });
